7ace151ab67acd120c08338b227348e2ad0575e1
Andy Stanford-Bluntish <andy@bluntish.net>
2021-12-07 22:22:16 +1030

    cleanup

--- Day 02 - Dive!/lib_test.ts
+++ Day 02 - Dive!/lib_test.ts
@@ -1,26 +1,19 @@
 import { assertEquals } from "https://deno.land/std@0.116.0/testing/asserts.ts";
 import { ex1, ex2 } from "./lib.ts";
 
+const input = [
+  "forward 5",
+  "down 5",
+  "forward 8",
+  "up 3",
+  "down 8",
+  "forward 2",
+];
+
 Deno.test("ex 1", () => {
-  const input = [
-    "forward 5",
-    "down 5",
-    "forward 8",
-    "up 3",
-    "down 8",
-    "forward 2",
-  ];
   assertEquals(ex1(input), 150);
 });
 
 Deno.test("ex 2", () => {
-  const input = [
-    "forward 5",
-    "down 5",
-    "forward 8",
-    "up 3",
-    "down 8",
-    "forward 2",
-  ];
   assertEquals(ex2(input), 900);
 });
